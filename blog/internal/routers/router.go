cbcab405cd40efe3e6d6b0f8c1f85e1cdf85b13e
lujet <462546997@qq.com>
2022-08-30 13:48:45 +0800

    blog 项目结构设计

---
+++ blog/internal/routers/router.go
@@ -0,0 +1,34 @@
+package routers
+
+import (
+	v1 "blog/internal/routers/api/v1"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter() *gin.Engine {
+	r := gin.New()
+
+	article := v1.NewArticle()
+	tag := v1.NewTag()
+
+	apiv1 := r.Group("/api/v1")
+	{
+		// tag
+		apiv1.POST("/tags", tag.Create)
+		apiv1.DELETE("/tags/:id", tag.Delete)
+		apiv1.PUT("/tags/:id", tag.Update)
+		apiv1.PATCH("/tags/:id/state", tag.Update)
+		apiv1.GET("/tags", tag.List)
+
+		// article
+		apiv1.POST("/articles", article.Create)
+		apiv1.DELETE("/articles/:id", article.Delete)
+		apiv1.PUT("/articles/:id", article.Update)
+		apiv1.PATCH("/articles/:id/state", article.Update)
+		apiv1.GET("/articles/:id", article.Get)
+		apiv1.GET("/articles", article.List)
+	}
+
+	return r
+}
--- blog/main.go
+++ blog/main.go
@@ -11,12 +11,20 @@ package main
 
 */
 
-import "github.com/gin-gonic/gin"
+import (
+	"blog/internal/routers"
+	"net/http"
+	"time"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
-	})
-	r.Run()
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 10, // 20MB
+	}
+	s.ListenAndServe()
 }
